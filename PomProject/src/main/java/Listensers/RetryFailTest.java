94c8f07b6e538bc16ba393c736f6d79348f886c9
Rahul <ramagirirahul12@gmail.com>
2024-05-16 16:52:52 +0530

    IRetryAnalyzer

---
+++ PomProject/src/main/java/Listensers/RetryFailTest.java
@@ -0,0 +1,21 @@
+package Listensers;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryFailTest  implements IRetryAnalyzer{
+
+
+	private int retryCount = 0;
+	private static final int MAX_RETRY_COUNT = 3; // Maximum number of retrie
+	@Override
+	public boolean retry(ITestResult result) {
+		if (retryCount < MAX_RETRY_COUNT) {
+            retryCount++;
+            return true; // Retry the test
+        }
+        return false;
+
+	}
+
+}
